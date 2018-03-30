3f25391c459979ed876e8f9177038dc334f46fb9
Kyle <kyle.j.yount@gmail.com>
2018-03-29 20:53:43 -0700

    small change

--- src/script.js
+++ src/script.js
@@ -1,5 +1,47 @@
-var widget;
-var mobileInterval;
+var projectObj = [
+  {
+    id: 0,
+    selected: false,
+    title: "Project 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
+    images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
+  },
+  {
+    id: 1,
+    selected: false,
+    title: "Project 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
+    images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
+  },
+  {
+    id: 2,
+    selected: false,
+    title: "Project 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
+    images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
+  },
+  {
+    id: 3,
+    selected: false,
+    title: "Project 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
+    images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
+  },
+  {
+    id: 4,
+    selected: false,
+    title: "Project 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
+    images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
+  },
+  {
+    id: 5,
+    selected: false,
+    title: "Project 5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
+    images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
+  }
+];
 
 var vm = new Vue({
   el: '#app',
@@ -8,50 +50,7 @@ var vm = new Vue({
     contactBox: false,
     isProjectShowing: true,
     activeProject: 0,
-    projects: [
-      {
-        id: 0,
-        selected: false,
-        title: "Project 1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-        images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
-      },
-      {
-        id: 1,
-        selected: false,
-        title: "Project 2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-        images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
-      },
-      {
-        id: 2,
-        selected: false,
-        title: "Project 3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-        images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
-      },
-      {
-        id: 3,
-        selected: false,
-        title: "Project 3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-        images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
-      },
-      {
-        id: 4,
-        selected: false,
-        title: "Project 4",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-        images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
-      },
-      {
-        id: 5,
-        selected: false,
-        title: "Project 5",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-        images: ['http://placehold.it/600x400', 'http://placehold.it/600x400', 'http://placehold.it/600x400'],
-      }
-    ]
+    projects: projectObj
   },
   computed: {
     isMobileDevice: function() {
