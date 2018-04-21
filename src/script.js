85e392c657b04a70a2d00f38e8ccc33b111b41e6
Kyle <kyle.j.yount@gmail.com>
2018-04-21 15:15:38 -0700

    changing the css and stuff to make it transition

--- src/script.js
+++ src/script.js
@@ -20,7 +20,7 @@ var projectObj = [
   {
     id: 2,
     selected: false,
-    title: "PROJECT 4",
+    title: "PROJECT 3",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/603/400/?random',
     width: 600,
@@ -30,7 +30,7 @@ var projectObj = [
   {
     id: 3,
     selected: false,
-    title: "PROJECT 5",
+    title: "PROJECT 4",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/604/400/?random',
     width: 600,
@@ -40,7 +40,7 @@ var projectObj = [
   {
     id: 4,
     selected: false,
-    title: "PROJECT 6",
+    title: "PROJECT 5",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/605/400/?random',
     width: 600,
@@ -50,7 +50,7 @@ var projectObj = [
   {
     id: 5,
     selected: false,
-    title: "PROJECT 5",
+    title: "PROJECT 6",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/606/400/?random',
     images: ['https://picsum.photos/606/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random'],
