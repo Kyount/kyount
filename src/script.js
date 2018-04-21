e8532737e9c07a7f2987310788c107a5a4009b39
Kyle <kyle.j.yount@gmail.com>
2018-04-21 16:36:09 -0700

    Finished main menu transition basics

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ var projectObj = [
   {
     id: 0,
     selected: false,
-    title: "PROJECT 1",
+    title: "ABOUT",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/601/400/?random',
     images: ['https://picsum.photos/601/400/?random'],
@@ -10,7 +10,7 @@ var projectObj = [
   {
     id: 1,
     selected: false,
-    title: "PROJECT 2",
+    title: "PROJECTS",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/602/400/?random',
     width: 600,
@@ -20,7 +20,7 @@ var projectObj = [
   {
     id: 2,
     selected: false,
-    title: "PROJECT 3",
+    title: "PHOTOGRAPHY",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/603/400/?random',
     width: 600,
@@ -30,30 +30,12 @@ var projectObj = [
   {
     id: 3,
     selected: false,
-    title: "PROJECT 4",
+    title: "CONTACT",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
     thumbnail: 'https://picsum.photos/604/400/?random',
     width: 600,
     height: 400,
     images: ['https://picsum.photos/604/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random'],
-  },
-  {
-    id: 4,
-    selected: false,
-    title: "PROJECT 5",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-    thumbnail: 'https://picsum.photos/605/400/?random',
-    width: 600,
-    height: 400,
-    images: ['https://picsum.photos/605/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random'],
-  },
-  {
-    id: 5,
-    selected: false,
-    title: "PROJECT 6",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec elit quam. Sed euismod lorem dui. Vestibulum ultrices accumsan scelerisque. Morbi sit amet odio libero. Fusce scelerisque elit nulla, sed fermentum libero lacinia at. Nam ultricies enim vel ante iaculis laoreet eu dignissim neque. Aliquam id metus dapibus, pharetra nisi id, tincidunt est. Nam magna ex, sagittis eu lorem quis, fringilla convallis dolor. Integer id nibh eros. Etiam vehicula, arcu et lacinia fermentum, dui est pellentesque nisl, vitae hendrerit lacus mauris sit amet neque. Quisque ultrices sodales enim. Duis eu sapien mollis, dapibus arcu viverra, consequat nisl. Nam lectus lectus, tempus vitae neque ut, pharetra blandit dui.",
-    thumbnail: 'https://picsum.photos/606/400/?random',
-    images: ['https://picsum.photos/606/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random', 'https://picsum.photos/600/400/?random'],
   }
 ];
 
